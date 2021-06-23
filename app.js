66bef26c9180f48cc8f9e326c2c8eb0ff439d1e2
mikemartinezr7 <xn1p3r@gmail.com>
2021-06-22 21:03:47 -0600

    app.js modified with changes in how to get the port, bak to the original

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require('express');
+const port = process.env.PORT || 3000;
 const app  = express();
-app.set('port', (process.env.PORT || 5000));
 
 userRouter = require('./routes/userRoutes')();
 
@@ -10,6 +10,6 @@ app.get('/api', (req, res) => {
     res.send('Hello from the API');
 });
 
-app.listen(app.get('port'), function () {
-  console.log('Listening on port ' + app.get('port') + '/');
+app.listen(port, () => {
+    console.log(`Listening on port ${port}`);
 });
