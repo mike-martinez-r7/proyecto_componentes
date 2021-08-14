b815b878b9ed36aa2af61f0bb4803539a74382d2
mikemartinezr7 <xn1p3r@gmail.com>
2021-08-14 12:34:12 -0600

    Second try to anable CORS on API

--- api/app.js
+++ api/app.js
@@ -1,16 +1,12 @@
 const express = require('express');
+var cors      = require('cors')
 userRouter    = require('./routes/userRoutes')();
 companyRouter = require('./routes/companyRoutes')();
 
 const port = process.env.PORT || 3000;
 const app  = express();
 
-app.use(function(req, res, next) {
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-  next();
-});
-
+app.use(cors())
 app.use(express.json());
 app.use('/api/users', userRouter);
 app.use('/api/companies', companyRouter);
